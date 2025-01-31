a8ca2d24e53ea13f095ab38111d8e8105ced4c12
Sunil Kumar <sunilkumarsk8899@gmail.com>
2025-01-31 18:58:25 +0530

    Authentication

--- my-invoice-app/resources/js/components/invoices/index.vue
+++ my-invoice-app/resources/js/components/invoices/index.vue
@@ -1,5 +1,72 @@
 <template>
-    <h1>
-        Index page
-    </h1>
+<div class="container">
+    <div class="invoices">
+
+        <div class="card__header">
+            <div>
+                <h2 class="invoice__title">Invoices</h2>
+            </div>
+            <div>
+                <a class="btn btn-secondary">
+                    New Invoice
+                </a>
+            </div>
+        </div>
+
+        <div class="table card__content">
+            <div class="table--filter">
+                <span class="table--filter--collapseBtn ">
+                    <i class="fas fa-ellipsis-h"></i>
+                </span>
+                <div class="table--filter--listWrapper">
+                    <ul class="table--filter--list">
+                        <li>
+                            <p class="table--filter--link table--filter--link--active">
+                                All
+                            </p>
+                        </li>
+                        <li>
+                            <p class="table--filter--link ">
+                                Paid
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="table--search">
+                <div class="table--search--wrapper">
+                    <select class="table--search--select" name="" id="">
+                        <option value="">Filter</option>
+                    </select>
+                </div>
+                <div class="relative">
+                    <i class="table--search--input--icon fas fa-search "></i>
+                    <input class="table--search--input" type="text" placeholder="Search invoice">
+                </div>
+            </div>
+
+            <div class="table--heading">
+                <p>ID</p>
+                <p>Date</p>
+                <p>Number</p>
+                <p>Customer</p>
+                <p>Due Date</p>
+                <p>Total</p>
+            </div>
+
+            <!-- item 1 -->
+            <div class="table--items">
+                <a href="#" class="table--items--transactionId">#093654</a>
+                <p>Jan 18, 9:31am</p>
+                <p>#093654</p>
+                <p>Jonathan Yu</p>
+                <p>Jan 18, 9:31am</p>
+                <p> $ 16,943</p>
+            </div>
+        </div>
+
+    </div>
+
+</div>
 </template>
--- my-invoice-app/resources/views/app.blade.php
+++ my-invoice-app/resources/views/app.blade.php
@@ -4,18 +4,18 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Styles / Scripts -->
-        @vite('resources/css/app.css')
-
+        <!-- Scripts -->
+        @routes
+        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+        @inertiaHead
     </head>
-    <body>
-        <div id="app"></div>
-        @vite('resources/js/app.js')
+    <body class="font-sans antialiased">
+        @inertia
     </body>
 </html>
